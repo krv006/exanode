00e205c60ad8c4cb60791ef0f833df0fe26a791b
krv006 <rvkamronbek@gmail.com>
2025-12-31 22:37:21 +0500

    change

--- apps/admin.py
+++ apps/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-from . import models
+from django.contrib.admin import ModelAdmin
 
+from .models import Contact
+
+
+@admin.register(Contact)
+class ContactAdmin(ModelAdmin):
+    list_display = 'full_name', 'email', 'phone_number', 'company'
+    list_filter = 'email', 'phone_number', 'company'
--- apps/models.py
+++ apps/models.py
@@ -5,7 +5,7 @@ class Contact(Model):
     full_name = CharField(max_length=500)
     email = CharField(max_length=500)
     subject = CharField(max_length=250)
-
+    phone_number = CharField(max_length=250)
     company = CharField(max_length=100, null=True, blank=True)
     message = TextField(null=True, blank=True)
 
